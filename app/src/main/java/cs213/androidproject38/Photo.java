df1da57b6d0dbb4684e8db0307189b0aa97caaf6
Daoun Oh <daounski@gmail.com>
2016-05-02 15:32:08 -0400

    viewimage tags functional

--- app/src/main/java/cs213/androidproject38/Photo.java
+++ app/src/main/java/cs213/androidproject38/Photo.java
@@ -4,7 +4,6 @@ import android.graphics.Bitmap;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -13,30 +12,13 @@ import java.util.List;
 public class Photo implements Serializable {
 
     private static final long serialVersionUID = 2651057460302515220L;
-    private List<String> taglist = new ArrayList<String>();;
+    private List<String> locationTagList = new ArrayList<>();
+    private List<String> personTagList = new ArrayList<>();
+
     private Bitmap url;
-    private String caption;
-    private Date date;
 
     public Photo(Bitmap url){
         this.url = url;
-    }
-
-    public String getCaption() {
-        return caption;
-    }
-    public void setCaption(String caption) {
-        this.caption = caption;
-    }
-
-    public String getTag(int index) {
-        return taglist.get(index);
-    }
-    public void addTag(String tag) {
-        this.taglist.add(tag);
-    }
-    public int getTaglistSize(){
-        return taglist.size();
     }
 
     public Bitmap getURL() {
@@ -46,23 +28,19 @@ public class Photo implements Serializable {
         this.url = url;
     }
 
-    public Date getDate() {
-        return date;
+    public List<String> getLocationTaglist() {
+        return locationTagList;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public void setLocationTaglist(List<String> taglist) {
+        this.locationTagList = taglist;
     }
 
-    public String toString(){
-        return caption;
+    public List<String> getPersonTaglist(){
+        return personTagList;
     }
 
-    public List<String> getTaglist() {
-        return taglist;
-    }
-
-    public void setTaglist(List<String> taglist) {
-        this.taglist = taglist;
+    public void setPersonTaglist(List<String> taglist){
+        this.personTagList = taglist;
     }
 }
--- app/src/main/java/cs213/androidproject38/ViewImage.java
+++ app/src/main/java/cs213/androidproject38/ViewImage.java
@@ -1,26 +1,52 @@
 package cs213.androidproject38;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewImage extends AppCompatActivity {
     //Test
-    ImageView iv3;
+    ImageView photoIV;
 
     public static int currPhoto;
+    
+    private Photo photo;
+    String personTagString;
+    List<String> personTagList = new ArrayList<>();
+    String locationTagString;
+    List<String> locationTagList = new ArrayList<>();
+
+    TextView locationTV;
+    TextView personTV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_image);
 
-        //String f = getIntent().getStringExtra("img");
-        //iv3 = (ImageView) findViewById(R.id.imageView3);
-        //iv3.setImageURI(Uri.parse(f));
+        photo = Home.albumList.get(Photos.currAlbum).getPhoto(currPhoto);
+
+        photoIV = (ImageView) findViewById(R.id.photoIV);
+        photoIV.setImageBitmap(photo.getURL());
+
+        locationTV = (TextView) findViewById(R.id.locationTagTV);
+        locationTV.setText( combineTags(photo.getLocationTaglist()) );
+
+        personTV = (TextView) findViewById(R.id.personTagTV);
+        personTV.setText( combineTags(photo.getPersonTaglist()) );
+
 
     }
 
@@ -46,13 +72,6 @@ public class ViewImage extends AppCompatActivity {
                 addLocationTagAction();
                 return true;
 
-            case R.id.deleteTagAction:
-                deleteTagAction();
-                return true;
-
-            case R.id.displayInfoAction:
-                displayInfoAction();
-                return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -60,15 +79,103 @@ public class ViewImage extends AppCompatActivity {
 
 
     public void addPersonTagAction(){
+        AlertDialog.Builder prompt = new AlertDialog.Builder(this);
+        prompt.setTitle("Person Tag");
+        prompt.setMessage("Add and modify PERSON tags (separate each tag with #)");
 
+        // Set up the input
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        input.setPadding(40, 25, 40, 25);
+        input.setText(combineTags(photo.getPersonTaglist()));
+
+        prompt.setView(input);
+
+        // Set up the buttons
+        prompt.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                personTagString = input.getText().toString();
+                personTagList = parseTags(personTagString);
+                Home.albumList.get(Photos.currAlbum).getPhoto(currPhoto).setPersonTaglist(personTagList);
+                personTV.setText(combineTags(personTagList));
+            }
+        });
+        prompt.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        prompt.show();
     }
     public void addLocationTagAction(){
+        AlertDialog.Builder prompt = new AlertDialog.Builder(this);
+        prompt.setTitle("Location Tag");
+        prompt.setMessage("Add and modify LOCATION tags (separate each tag with #)");
 
-    }
-    public void deleteTagAction(){
+        // Set up the input
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        input.setPadding(40, 25, 40, 25);
+        input.setText(combineTags(photo.getLocationTaglist()));
 
-    }
-    public void displayInfoAction(){
+        prompt.setView(input);
 
+        // Set up the buttons
+        prompt.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                locationTagString = input.getText().toString();
+                locationTagList = parseTags(locationTagString);
+                Home.albumList.get(Photos.currAlbum).getPhoto(currPhoto).setLocationTaglist(locationTagList);
+                locationTV.setText(combineTags(locationTagList));
+            }
+        });
+        prompt.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        prompt.show();
     }
+
+    public List<String> parseTags(String input){
+        List<String> result = new ArrayList<>();
+
+
+        input=input.replaceAll("\\s+","");
+        input=input.replaceAll("\\s","");
+
+        for (String retval: input.split("#")){
+            result.add(retval);
+        }
+        for(int i = result.size()-1; i >= 0; i--){
+            if(result.get(i).equals("") || result.get(i).equals(" ")){
+                result.remove(i);
+            }
+        }
+
+
+        return result;
+    }
+
+    public String combineTags(List<String> taglist){
+        String result = "";
+
+        for(int i = 0; i <taglist.size(); i++){
+            result = result + "#" + taglist.get(i) + " ";
+        }
+
+        return result;
+    }
+
+
+
+
 }
